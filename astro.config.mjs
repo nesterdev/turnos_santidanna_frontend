2742a44a45a2b895834a398a0744a65356fdbac5
nesterdev <nesterlon.dev@gmail.com>
2026-08-25 21:11:28 -0500

    sasss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 import path from "path";
+
 export default defineConfig({
-  output: "static", // 🔥 CLAVE: build estático
+  output: "static",
   integrations: [react()],
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve('./src')
